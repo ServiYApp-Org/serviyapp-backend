44611b2081912d40477fe95bbdf98444c49fe6ff
Bruno Ramos Mejia <103078216+BrunoRamosMejia@users.noreply.github.com>
2025-10-29 21:25:03 -0300

    Implementacion Documentacion API (#15)
    -Agregada documentacion en el endpoint '/docs' (para ver los cambios reflejados en este se debe re-lanzar la app)
    -Pequeña prueba de manejo de ramas y branches

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDoc = new DocumentBuilder()
+    .setTitle('ServiYApp | Documentacion de API')
+    .setDescription('API para la gestión de servicios, usuarios y reservas en la plataforma ServiYApp. Incluye endpoints para autenticación, administración y operaciones de servicios.')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentModule = SwaggerModule.createDocument(app, swaggerDoc);
+  SwaggerModule.setup('docs', app, documentModule)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
