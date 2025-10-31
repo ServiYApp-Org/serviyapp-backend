69f1c714968878ab2604eef17c8b3d0b3697ea50
Diego Acuna <diegoacuna.dev@gmail.com>
2025-10-31 16:59:52 -0500

    Configuracion del main para render

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const swaggerDoc = new DocumentBuilder()
+    .setTitle('ServiYApp | Documentacion de API')
+    .setDescription(
+      'API para la gestión de servicios, usuarios y reservas en la plataforma ServiYApp. Incluye endpoints para autenticación, administración y operaciones de servicios.',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentModule = SwaggerModule.createDocument(app, swaggerDoc);
+  SwaggerModule.setup('docs', app, documentModule);
+
+  // 🔥 Importante: Render necesita que el servidor escuche en 0.0.0.0
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 ServiYApp backend is running on port ${port}`);
 }
 bootstrap();
